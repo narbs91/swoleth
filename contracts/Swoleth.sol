127383ee5d07a41e82ae0b7ea8bf7ac467a5a782
Narbeh Shahnazarian <megatron@Narbehs-MacBook-Pro.local>
2021-08-08 21:32:03 -0700

    adds test dependicies and base; made slight tweaks to contract; added openzepplin test-helpers

--- contracts/Swoleth.sol
+++ contracts/Swoleth.sol
@@ -1,6 +1,108 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.8.0 <0.9.0;
 
-contract Swoleth {
-    
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+    @dev contract representing an database for exercises meant to be the decentralized source of truth for fitness based dApps
+ */
+contract Swoleth is Ownable {
+
+    event NewExercise(string muscleGroup, string name);
+
+    // Represents the muscle groups of the human body with corresponding exercies
+    struct ExercisesByMuscleGroup {
+        // name
+        string muscleGroup;
+        uint32 exerciseCount;
+        mapping (uint=>Exercise) exercies;
+    }
+
+    // Respesents a single exercise
+    struct Exercise {
+        // name, description, category
+        string name;
+        string description;
+        string category;
+    }
+
+    // Represents the human body made up of muscle groups
+    mapping (string => ExercisesByMuscleGroup) anatomy;
+    mapping (string => Exercise) public exercises;
+
+    // Metadata for exercises
+    mapping (string => string) public exerciseCategory;
+    mapping (string => string) public exerciseMuscleGroup;
+
+    address private _owner;
+
+    constructor() {
+        _owner  = msg.sender;
+
+        exerciseCategory["MACHINE"] = "MACHINE";
+        exerciseCategory["BARBELL"] = "BARBELL";
+        exerciseCategory["DUMBBELL"] = "DUMBBELL";
+        exerciseCategory["BODYWEIGHT"] = "BODYWEIGHT";
+        exerciseCategory["BANDS"] = "BANDS";
+        
+        exerciseMuscleGroup["BACK"] = "BACK";
+        exerciseMuscleGroup["CHEST"] = "CHEST";
+        exerciseMuscleGroup["LEGS"] = "LEGS";
+        exerciseMuscleGroup["ARMS"] = "ARMS";
+        exerciseMuscleGroup["CORE"] = "CORE";
+    }
+
+    /**
+        @dev function to add an exercise with a name, description, category and muscle group
+     */
+    function addExercise(string memory _name, 
+                        string memory _description, 
+                        string memory _category, 
+                        string memory _muscleGroup) public onlyOwner {
+        require(keccak256(abi.encodePacked(exercises[_name].name)) != keccak256(abi.encodePacked(_name)));
+        require(keccak256(abi.encodePacked(exerciseCategory[_category])) == keccak256(abi.encodePacked(_category)));
+        require(keccak256(abi.encodePacked(exerciseMuscleGroup[_muscleGroup])) == keccak256(abi.encodePacked(_muscleGroup)));
+
+        Exercise memory exercise = Exercise(_name, _description, _category);
+        exercises[_name] = exercise;
+
+        //addExerciseToMuscleGroup(_muscleGroup, exercise);
+        emit NewExercise(_muscleGroup, _name);
+    }
+
+    /**
+        @dev function to add an exercise to a muscle group
+     */
+    function addExerciseToMuscleGroup(string memory _muscleGroup, Exercise memory _exercise) private onlyOwner {
+        ExercisesByMuscleGroup storage exerciseByMuscleGroup = anatomy[_muscleGroup];
+        uint32 exerciseLength = exerciseByMuscleGroup.exerciseCount;
+
+        if (exerciseLength == 0) {
+            exerciseByMuscleGroup.exercies[exerciseLength] = _exercise;
+        } else {
+            exerciseByMuscleGroup.exercies[exerciseLength+1] = _exercise;
+        }
+
+        exerciseLength++;
+        exerciseByMuscleGroup.exerciseCount = exerciseLength;
+
+        emit NewExercise(_muscleGroup, _exercise.name);
+    }
+
+    function getExerciseCategory(string memory _category) public view returns (string memory) {
+        return exerciseCategory[_category];
+    }
+
+    function getExerciseMuscleGroup(string memory _muscleGroup) public view returns (string memory) {
+        return exerciseMuscleGroup[_muscleGroup];
+    }
+
+    function getExercise(string memory _name) public view returns (string memory, string memory, string memory) {
+        Exercise storage exercise = exercises[_name];
+        return (exercise.name, exercise.description, exercise.category);
+    }
+
+    function ownerOf() public view returns (address) {
+        return _owner;
+    }
 }
--- test/Swoleth.js
+++ test/Swoleth.js
@@ -1,16 +1,75 @@
+const { assert, expect } = require("chai");
+const { expectRevert } = require('@openzeppelin/test-helpers');
+
 var Swoleth = artifacts.require("./Swoleth.sol");
 
 contract('Swoleth', accounts => {
+    let swolethInstance;
+    let [alice, bob] = accounts;
 
-    it("passes", () =>
-    Swoleth.deployed()
-      .then(() => {
-        assert.equal(
-          true,
-          true,
-          "Tiss false"
-        );
-      }));
+    beforeEach(async () => {
+      swolethInstance = await Swoleth.new({from: alice});
+    })
+
+    context("Fresh contract deployment scenario", async () => {
+      it("It should have categories", async () => {
+        const machineCategory = await swolethInstance.getExerciseCategory.call("MACHINE");
+        expect(machineCategory).to.equal("MACHINE", "Categories not properly loaded");
+      });
 
 
+      it("should have muscle groups", async () => {
+        const chestMuscleGorup = await swolethInstance.getExerciseMuscleGroup.call("CHEST");
+        expect(chestMuscleGorup).to.equal("CHEST", "MusclesGroups not properly set");
+      });
+
+      it("should have the correct owner", async () => {
+        const owner = await swolethInstance.ownerOf.call();
+        expect(owner).to.equal(alice);
+      })
+    })
+
+    context("Creating exercises by owner", async () => {
+      it("should insert an exercise", async () => {
+        const exercise = {
+          name: "Biscep curl",
+          description: "Arm exercise",
+          category: "DUMBBELL",
+          muscleGroup: "ARMS"
+        };
+
+        await swolethInstance.addExercise(exercise.name, exercise.description, exercise.category, exercise.muscleGroup)
+        .then(function(result) {
+          expect(result.receipt.status).to.equal(true, "Transaction was not successful");
+          //console.log(result);
+          expect(result.logs[0].args.muscleGroup).to.equal(exercise.muscleGroup, "Incorrect muscle group was returned");
+          expect(result.logs[0].args.name).to.equal(exercise.name, "Incorrect exercise name was returned");
+        })
+      });
+    })
+
+
+    context("Testing bad exercise creations", async () => {
+
+      const exercise = {
+        name: "Biscep curl",
+        description: "Arm exercise",
+        category: "fake",
+        muscleGroup: "fake"
+      };
+
+      it("should not insert an exercise by bob", async () => {
+        await expectRevert(swolethInstance.addExercise.call(exercise.name, exercise.description, exercise.category, exercise.muscleGroup, {from: bob}), "Ownable: caller is not the owner");
+      });
+
+      it("should not insert an exercise with a non-existent category", async () => {
+        await expectRevert(swolethInstance.addExercise.call(exercise.name, exercise.description, exercise.category, exercise.muscleGroup), "revert");
+      });
+
+      it("should not insert an exercise with a non-existent muscle group", async () => {
+        await expectRevert(swolethInstance.addExercise.call(exercise.name, exercise.description, exercise.category, exercise.muscleGroup), "revert");
+      });
+
+      //TODO: how to test a duplicate exercise entry?
+    })
 })
